b68bf6e87a658febbe072415782d536b982ecb57
Mark Ajtay <one-maaj@onead.dk>
2020-10-20 16:14:11 +0200

    fixes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const Nightmare = require('nightmare'),
-    nightmare = Nightmare({ show: true }),
+const initNightmare = require('nightmare'),
     Xvfb = require('xvfb'),
+    cron = require('node-cron'),
     xvfb = new Xvfb(),
     winston = require('winston'),
-    now = new Date();
-
-try {
-    xvfb.startSync();
-}
-catch (e) {
-    console.log(e);
-}
+    nightmareOptions = {
+        gotoTimeout: 10000,
+        loadTimeout: 15000,
+        show: false
+    };
 
 const logger = winston.createLogger({
     format: winston.format.simple(),
@@ -20,29 +17,55 @@ const logger = winston.createLogger({
     ],
 });
 
-nightmare
-    .goto('http://192.168.0.1/')
-    .wait('input[name=username]')
-    .insert('input[name=username]', 'admin')
-    .insert('input[name=password]', 'passwordhere!')
-    .click('input[value="Log in"]')
-    .wait(2000)
-    .evaluate(() => {
-        var allButtons = document.getElementsByTagName('a');
-        for (var i = 0; i < allButtons.length; i++) {
-            if (allButtons[i].innerText == 'Reset') allButtons[i].id = 'resetbutton';
-        }
-    })
-    .click('a[id=resetbutton]')
-    .wait('input[value=Reboot]')
-    .click('input[value=Reboot]')
-    .wait('input[value=Ok]')
-    //.click('input[value=Ok]')
-    .end()
-    .then(() => {
-        logger.info(`[${now}] Router restarted successfully.`)
-        console.log('Router restarted successfully at ' + now)
-    })
-    .catch(err => {
-        console.error(`[${now}] An unknown error occured. ${err}`)
-    })
+/* GET NEW NIGHTMARE INSTANCE ON EVERY RUN */
+function getNewNightmare() {
+    return initNightmare(nightmareOptions)
+}
+
+/* RUN EVERY N HOURS */
+cron.schedule('* * * * *', () => {
+    let newNightmare = getNewNightmare();
+
+    // try {
+    //     xvfb.startSync();
+    // }
+    // catch (e) {
+    //     console.log(e);
+    // }
+
+    logger.info(`[${new Date}] Router reboot initiated...`)
+    //console.log('Rebooting...');
+
+    newNightmare
+        .goto('http://192.168.0.1/')
+        .wait('input[name=username]')
+        .insert('input[name=username]', 'admin')
+        .insert('input[name=password]', 'password')
+        .click('input[value="Log in"]')
+        .wait(2000)
+        .evaluate(() => {
+            var allButtons = document.getElementsByTagName('a');
+            for (var i = 0; i < allButtons.length; i++) {
+                if (allButtons[i].innerText == 'Reset') allButtons[i].id = 'resetbutton';
+            }
+        })
+        .click('a[id=resetbutton]')
+        .wait('input[value=Reboot]')
+        .click('input[value=Reboot]')
+        .wait('input[value=Ok]')
+        //.click('input[value=Ok]')
+        .cookies.clearAll()
+        .end()
+        .then(() => {
+            logger.info(`[${new Date}] Router rebooted successfully.`)
+            //console.log('Router rebooted successfully at ' + new Date)
+            newNightmare = null;
+        })
+        .catch(err => {
+            logger.info(`[${new Date}] An unknown error occured. ${err}`)
+            newNightmare = null;
+        })
+
+
+});
+
