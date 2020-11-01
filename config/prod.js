347e7bffbe5be1b7a4f99654c4267b9c63f7164f
marcusique <markajtay@gmail.com>
2020-11-01 21:13:30 +0100

    prep for caprover

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  routerURL: process.env.ROUTER_URL,
+  routerUsername: process.env.ROUTER_USERNAME,
+  routerPwd: process.env.ROUTER_PWD,
+};
--- index.js
+++ index.js
@@ -1,73 +1,71 @@
 const initNightmare = require('nightmare'),
-    Xvfb = require('xvfb'),
-    cron = require('node-cron'),
-    xvfb = new Xvfb(),
-    winston = require('winston'),
-    nightmareOptions = {
-        gotoTimeout: 10000,
-        loadTimeout: 15000,
-        show: false
-    };
+  Xvfb = require('xvfb'),
+  keys = require('./config/keys'),
+  cron = require('node-cron'),
+  xvfb = new Xvfb(),
+  winston = require('winston'),
+  nightmareOptions = {
+    gotoTimeout: 10000,
+    loadTimeout: 15000,
+    show: false,
+  };
 
 const logger = winston.createLogger({
-    format: winston.format.simple(),
-    transports: [
-        //new winston.transports.File({ filename: 'router-reboot.log' }),
-        new winston.transports.File({ filename: '/var/log/router-reboot.log' })
-    ],
+  format: winston.format.simple(),
+  transports: [
+    //new winston.transports.File({ filename: 'router-reboot.log' }),
+    new winston.transports.File({ filename: '/var/log/router-reboot.log' }),
+  ],
 });
 
 /* GET NEW NIGHTMARE INSTANCE ON EVERY RUN */
 function getNewNightmare() {
-    return initNightmare(nightmareOptions)
+  return initNightmare(nightmareOptions);
 }
 
 /* RUN EVERY N HOURS */
-cron.schedule('* * * * *', () => {
-    let newNightmare = getNewNightmare();
+cron.schedule('59 6 * * *', () => {
+  let newNightmare = getNewNightmare();
 
-    try {
-        xvfb.startSync();
-    }
-    catch (e) {
-        console.log(e);
-    }
+  try {
+    xvfb.startSync();
+  } catch (e) {
+    console.log(e);
+  }
 
-    logger.info(`[${new Date}] Router reboot initiated...`)
-    //console.log('Rebooting...');
+  logger.info(`[${new Date()}] Router reboot initiated...`);
+  console.log(`[${new Date()}] Router reboot initiated...`);
 
-    newNightmare
-        .goto('http://192.168.0.1/')
-        .wait('input[name=username]')
-        .insert('input[name=username]', 'admin')
-        .insert('input[name=password]', 'pyrzy5-bIzbid-nikxev')
-        .click('input[value="Log in"]')
-        .wait(2000)
-        .evaluate(() => {
-            var allButtons = document.getElementsByTagName('a');
-            for (var i = 0; i < allButtons.length; i++) {
-                if (allButtons[i].innerText == 'Reset') {
-                    allButtons[i].id = 'resetbutton';
-                }
-            }
-        })
-        .click('a[id=resetbutton]')
-        .wait('input[value=Reboot]')
-        .click('input[value=Reboot]')
-        .wait('input[value=Ok]')
-        //.click('input[value=Ok]')
-        .cookies.clearAll()
-        .end()
-        .then(() => {
-            logger.info(`[${new Date}] Router rebooted successfully.`)
-            //console.log('Router rebooted successfully at ' + new Date)
-            newNightmare = null;
-        })
-        .catch(err => {
-            logger.info(`[${new Date}] An error occured. ${err}`)
-            newNightmare = null;
-        })
-
-
+  newNightmare
+    .goto(keys.routerURL)
+    .wait('input[name=username]')
+    .insert('input[name=username]', keys.routerUsername)
+    .insert('input[name=password]', keys.routerPwd)
+    .click('input[value="Log in"]')
+    .wait(2000)
+    .evaluate(() => {
+      var allButtons = document.getElementsByTagName('a');
+      for (var i = 0; i < allButtons.length; i++) {
+        if (allButtons[i].innerText == 'Reset') {
+          allButtons[i].id = 'resetbutton';
+        }
+      }
+    })
+    .click('a[id=resetbutton]')
+    .wait('input[value=Reboot]')
+    .click('input[value=Reboot]')
+    .wait('input[value=Ok]')
+    .click('input[value=Ok]')
+    .cookies.clearAll()
+    .end()
+    .then(() => {
+      logger.info(`[${new Date()}] Router rebooted successfully.`);
+      console.log(`[${new Date()}] Router rebooted successfully.`);
+      newNightmare = null;
+    })
+    .catch((err) => {
+      logger.info(`[${new Date()}] An error occured. ${err}`);
+      console.log(`[${new Date()}] An error occured. ${err}`);
+      newNightmare = null;
+    });
 });
-
