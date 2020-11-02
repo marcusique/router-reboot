3713791594eb361bacbd40e1e58999e911777aa1
marcusique <markajtay@gmail.com>
2020-11-02 21:48:29 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const initNightmare = require('nightmare'),
   Xvfb = require('xvfb'),
   keys = require('./config/keys'),
   cron = require('node-cron'),
-  xvfb = new Xvfb(),
+  xvfb = new Xvfb(['-screen', '0', '1280x1024x24']),
   winston = require('winston'),
   nightmareOptions = {
     gotoTimeout: 10000,
